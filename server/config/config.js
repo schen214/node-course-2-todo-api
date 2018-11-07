eb06e8c9e1dd422c21784d7079d2e187ec9799a0
unknown <schen214@gmail.com>
2018-11-07 15:06:45 -0500

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -2,10 +2,22 @@
 var env =  process.env.NODE_ENV || 'development';
 console.log('env *****', env);
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
+if (env === 'development' || env === 'test') {
+  // When requiring a JSON file, it will automatically parse it to a javascript object
+  var config = require('./config.json');
+  // console.log(config);
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    console.log(process.env[key] = envConfig[key]);
+
+  });
 }
+
+// if (env === 'development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
+// } else if (env === 'test') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
+// }
